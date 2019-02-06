8e61e3b6e62d2a79aab3145af2897cfc92eef3c7
Danielle Platt <danielleplatt@hotmail.co.uk>
2019-02-06 17:17:38 +0000

    create enquiries

--- chalet/app/Http/Controllers/EnquiryController.php
+++ chalet/app/Http/Controllers/EnquiryController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Enquiry;
+use App\User;
 
 class EnquiryController extends Controller
 {
-    public function newEnquiry(Request $request)
+    /**
+     * Create a new entry in the enquiry table
+     */
+    public function create(Request $request)
     {
-        $enquiry = new Enquiry();
 
+        // Validate
         $rules = [
             'first_name' => 'required|max:100',
             'last_name' => 'required|max:100',
@@ -30,6 +34,34 @@ class EnquiryController extends Controller
         ];
     
         $this->validate($request, $rules, $customMessages);
+
+        // Get or create the associated user
+        $user = User::where('first_name', $request->first_name)
+                        ->where('last_name', $request->last_name)
+                        ->where('email', $request->email)
+                        ->first();
+
+        if($user == null){
+            $user = new User();
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            $user->email = $request->email;
+            $user->contact_number = $request->contact_number;
+            $user->save();
+        }
+
+        $enquiry = new Enquiry();
+
+        $enquiry->user_id = $user->id;
+        $enquiry->date_from = $request->date_from;
+        $enquiry->date_to = $request->date_to;
+        $enquiry->number_of_adults = $request->number_of_adults;
+        $enquiry->number_of_children = $request->number_of_children;
+        $enquiry->special_requirements = $request->special_requirements;
+        $enquiry->comments = $request->comments;
+        $enquiry->save();
+
+        return back();
 
     }
 }
