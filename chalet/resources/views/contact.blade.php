c31efbe213a886be37e5c683760e5a502ad659f4
Danielle Platt <danielleplatt@hotmail.co.uk>
2019-02-08 08:54:35 +0000

    update contact form

--- chalet/resources/views/contact.blade.php
+++ chalet/resources/views/contact.blade.php
@@ -3,16 +3,16 @@
 <form method="POST" action='/make-enquiry'>
     @csrf 
   <div class="form-group">
-    <label for="exampleInputEmail1">Full Name</label>
-    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
+    <label for="fullName">Full Name</label>
+    <input type="text" class="form-control" id="fullName" aria-describedby="emailHelp" placeholder="Enter name">
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Email Address</label>
-    <input type="email" class="form-control" id="exampleInputPassword1" placeholder="Email">
+    <label for="email">Email Address</label>
+    <input type="email" class="form-control" id="email" placeholder="Email">
   </div>
   <div class="form-group">
-   <label for="exampleFormControlTextarea1">Message</label>
-   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+   <label for="message">Message</label>
+   <textarea class="form-control" id="message" rows="3"></textarea>
  </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
