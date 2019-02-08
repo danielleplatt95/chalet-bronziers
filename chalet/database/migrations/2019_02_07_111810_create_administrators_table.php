30724da4b974ffed377b2137b6867fe8a0eb4a1a
Danielle Platt <danielleplatt@hotmail.co.uk>
2019-02-08 08:52:11 +0000

    add administrator funcitonality start

---
+++ chalet/database/migrations/2019_02_07_111810_create_administrators_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdministratorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('administrators', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('password');
+            $table->tinyInteger('can_approve_bookings');
+            $table->tinyInteger('can_approve_reviews');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('administrators');
+    }
+}
